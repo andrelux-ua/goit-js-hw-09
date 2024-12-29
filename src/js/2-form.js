dcf535cd176888b4717bdab824c9434731d1b920
Andriy Kiselyov <168927164+andrelux-ua@users.noreply.github.com>
2024-12-29 14:42:05 +0200

    homework-9

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,11 +1,8 @@
-// Ініціалізація об'єкта для збереження даних форми
 const formData = { email: '', message: '' };
 
-// Отримання елементів форми
 const form = document.querySelector('.feedback-form');
 const STORAGE_KEY = 'feedback-form-state';
 
-// Завантаження даних із локального сховища при завантаженні сторінки
 document.addEventListener('DOMContentLoaded', () => {
   const savedData = localStorage.getItem(STORAGE_KEY);
   if (savedData) {
@@ -21,28 +18,23 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 });
 
-// Збереження даних у локальне сховище під час введення
 form.addEventListener('input', event => {
   if (event.target.name === 'email' || event.target.name === 'message') {
-    formData[event.target.name] = event.target.value.trim(); // Зберігаємо обрізані значення
+    formData[event.target.name] = event.target.value.trim();
     localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
   }
 });
 
-// Обробка відправлення форми
 form.addEventListener('submit', event => {
   event.preventDefault();
 
-  // Перевірка, чи заповнені всі поля
   if (formData.email === '' || formData.message === '') {
     alert('Fill please all fields');
     return;
   }
 
-  // Логування об'єкта formData
   console.log(formData);
 
-  // Очищення локального сховища, об'єкта formData і форми
   localStorage.removeItem(STORAGE_KEY);
   formData.email = '';
   formData.message = '';
